98b8fe14877f1b10e8a24545ea754e83e2a74efd
Eko Candra <yteko12@gmail.com>
2022-03-10 15:05:55 +0700

    contoh whitespaces di kotlin

---
+++ src/main/kotlin/d_blokdankomentar.kt
@@ -0,0 +1,8 @@
+fun main(){
+    /*Blok digunakan untuk menulis ikatan sebuah statement yang dikelompokkan bersama menjadi satu bagian.
+    * blok di tandai dengan kurung kurawal*/
+
+    println("Hello, ini contoh blok!")
+
+    //Comment adalah baris komentar pada program yang tidak akan dieksekusi oleh compiler.
+}
