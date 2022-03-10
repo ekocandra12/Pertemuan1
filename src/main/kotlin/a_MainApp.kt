c5e398942676761678b2a681c2d65cae6410deac
Eko Candra <yteko12@gmail.com>
2022-03-10 14:43:25 +0700

    percobaan pertama kotlin

---
+++ src/main/kotlin/a_MainApp.kt
@@ -0,0 +1,5 @@
+fun main(){ /*fun main() adalah fungsi main diaplikasi Kotlin dimana sebagai titik awal ketika
+    program dijalankan.*/
+    println ("Hello, ini program pertama saya di kotlin!")
+    //println() adalah fungsi dasar untuk mencetak string ke layar.
+}
