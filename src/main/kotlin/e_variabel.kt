93a3aecd2226694b9fa4a0bef68ba63d54e6ace0
Eko Candra <yteko12@gmail.com>
2022-03-10 15:25:46 +0700

    contoh kode program penggunaan variabel

--- src/main/kotlin/e_variabel.kt
+++ src/main/kotlin/e_variabel.kt
@@ -4,5 +4,5 @@ fun main(){
 
     /**
     * Variabel adalah sebuah tempat/wadah didalam memori komputer untuk menyimpan data seperti
-data nama pengguna, kata sandi, data waktu, jumlah makanan dan lain-lain sebagainya.*/
+data nama pengguna, kata sandi, data waktu, jumlah makanan dan lain-lain.*/
 }
