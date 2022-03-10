dbe5c426d203832d4e34f24c6656b72790de5c41
Eko Candra <yteko12@gmail.com>
2022-03-10 15:28:20 +0700

    contoh kode program penggunaan variabel

--- src/main/kotlin/e_variabel.kt
+++ src/main/kotlin/e_variabel.kt
@@ -2,6 +2,9 @@ fun main(){
     var dynamicVar = 2
     println ("Contoh dynamic variabel = " + dynamicVar)
 
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar)
+
     /**
     * Variabel adalah sebuah tempat/wadah didalam memori komputer untuk menyimpan data seperti
 data nama pengguna, kata sandi, data waktu, jumlah makanan dan lain-lain.*/
