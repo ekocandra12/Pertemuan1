bef7eea8c43123a8f34bb8f1d974c1ee4e1ac12b
Eko Candra <yteko12@gmail.com>
2022-03-15 21:21:44 +0700

    contoh kode penerapan string

--- src/main/kotlin/p_string.kt
+++ src/main/kotlin/p_string.kt
@@ -9,4 +9,18 @@ fun main(args: Array<String>) {
      * String merupakan array dari karakter dan bersifat immutable (bersifat statis). Elemen suatu
     String dapat diakses dengan operasi indeks array contoh s[i] atau dengan menggunakan for-loop.
      */
+
+    var text = """
+        |Tell me and I forget.
+        |Teach me and I remember.
+        |Involve me and I learn.
+        |(Benjamin Franklin)
+    """.trimMargin()
+    println(text)
+
+    var teks = """
+        for (c in "foo")
+        print(c)
+    """
+    println(teks)
 }
